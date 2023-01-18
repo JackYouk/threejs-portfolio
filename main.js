ae1a07838e513d509c7f4336b1ea77e29e5f9c7b
JackYouk <jackyoukstetter1@gmail.com>
2023-01-17 20:57:19 -0800

    torus background

--- main.js
+++ main.js
@@ -14,45 +14,71 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(55);
-camera.position.setX(-5);
-camera.position.setY(15);
+camera.position.setX(-20);
+camera.position.setY(-2);
 renderer.render(scene, camera);
 
 // Torus
-// const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-// const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-// const torus = new THREE.Mesh(geometry, material);
-// scene.add(torus);
+const geometryT = new THREE.TorusGeometry(60, 60, 60, 60);
+const materialT = new THREE.MeshStandardMaterial({ color: 0xff6347, wireframe: true });
+const torus = new THREE.Mesh(geometryT, materialT);
+torus.position.setY(30)
+torus.position.setX(20)
+scene.add(torus);
+
+// Sphere
+// const geometryC = new THREE.SphereGeometry(5, 1.5, 8, 50);
+// const materialC = new THREE.MeshStandardMaterial({ color: 0xff6347, wireframe: true });
+// const sphere = new THREE.Mesh(geometryC, materialC);
+// sphere.position.setY(30)
+// sphere.position.setX(-220)
+// scene.add(sphere);
 
 // Text
 const fontLoader = new FontLoader()
 const ttfLoader = new TTFLoader();
-ttfLoader.load('TokaiFont.ttf', (json) => {
+ttfLoader.load('Roboto-Bold.ttf', (json) => {
   const tokaiFont = fontLoader.parse(json);
+    // drag to look around
+    const scrollGeometry = new TextGeometry('<- drag to look around ->', {
+      height: 0.2,
+      size: 1,
+      font: tokaiFont,
+    });
+    const scrollMaterial = new THREE.MeshBasicMaterial({color: 'white'});
+    const scroll = new THREE.Mesh(scrollGeometry, scrollMaterial);
 
   // name
   const nameGeometry = new TextGeometry('Jack Youkstetter', {
-    height: 4,
-    size: 10,
+    height: 1,
+    size: 6,
     font: tokaiFont,
   });
-  const nameMaterial = new THREE.MeshBasicMaterial({color: '0xffffff', wireframe: true});
+  const nameMaterial = new THREE.MeshBasicMaterial({color: 'lightblue'});
   const name = new THREE.Mesh(nameGeometry, nameMaterial);
 
   // title
   const titleGeometry = new TextGeometry('Web Developer and ML Engineer', {
     height: 0.5,
-    size: 3,
+    size: 2,
     font: tokaiFont,
   });
-  const titleMaterial = new THREE.MeshBasicMaterial({color: '0xffffff', wireframe: true});
+  const titleMaterial = new THREE.MeshBasicMaterial({color: 'gray'});
   const title = new THREE.Mesh(titleGeometry, titleMaterial);
 
-  title.position.setZ(20)
-  title.position.setX(-25)
+  name.position.setX(-40)
+  name.position.setY(0)
+  // title.position.setZ(20)
+  title.position.setX(-40)
+  title.position.setY(-5)
 
-  name.position.setX(-50)
+  scroll.position.setX(-10)
+  scroll.position.setY(10);
+  scroll.position.setZ(0);
+
   scene.add(name, title);
+
+
 });
 
 
@@ -60,31 +86,31 @@ ttfLoader.load('TokaiFont.ttf', (json) => {
 
 
 // Lights
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
+const pointLight = new THREE.PointLight();
+pointLight.position.set(20, 15, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // Helpers
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper)
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, )
 
 // controls
 const controls = new OrbitControls(camera, renderer.domElement);
-
 
 // animate function
 function animate() {
   requestAnimationFrame(animate);
 
-  // torus.rotation.x += 0.01;
-  // torus.rotation.y += 0.005;
-  // torus.rotation.z += 0.01;
-
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
 
   // controls.update();
+  // console.log(camera.position)
+
 
   renderer.render(scene, camera);
 }
